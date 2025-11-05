4289f440ead87cd1b08734ca87d6ac1c24891bd8
Luis Dementhon <luis.dementhon@gmail.com>
2025-11-05 03:57:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6613366678-b4207",
+  "appId": "1:557965420799:web:2766c5a04e3bd40f7384ca",
+  "apiKey": "AIzaSyCXqlZwo44HSS3BOVNTFP8o0xO4aOxwbVo",
+  "authDomain": "studio-6613366678-b4207.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "557965420799"
+};
