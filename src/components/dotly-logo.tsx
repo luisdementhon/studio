d3efc1119a6b6087423f60cf723337fa4d5e747e
Luis Dementhon <luis.dementhon@gmail.com>
2025-11-05 02:55:07 +0000

    le logo c ca enleve la piece garde juste les lettres dans la meme police

--- src/components/dotly-logo.tsx
+++ src/components/dotly-logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export function DotlyLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 452 91"
+      viewBox="0 0 350 91"
       xmlns="http://www.w3.org/2000/svg"
       aria-label="Dotly Logo"
       {...props}
@@ -19,9 +19,6 @@ export function DotlyLogo(props: SVGProps<SVGSVGElement>) {
         className={cn('font-headline')}
       >
         Dotly
-      </text>
-      <text x="350" y="85" fontSize="70">
-        💰
       </text>
     </svg>
   );
