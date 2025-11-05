8bdd3f954746f0301b0ef732389241944b2b2831
Luis Dementhon <luis.dementhon@gmail.com>
2025-11-05 02:58:27 +0000

    c pas du tout ca. regarde le logo

--- src/components/dotly-logo.tsx
+++ src/components/dotly-logo.tsx
@@ -9,30 +9,17 @@ export function DotlyLogo(props: SVGProps<SVGSVGElement>) {
       aria-label="Dotly Logo"
       {...props}
     >
-      <g fill="none" fillRule="evenodd">
         <text
           fontFamily="serif"
           fontSize="100"
-          fontWeight="bold"
+          fontWeight="normal"
           fill="currentColor"
           className={cn('font-headline')}
-          x="91"
-          y="67"
+          x="0"
+          y="75"
         >
           Dotly
         </text>
-        <g transform="translate(0 1)" stroke="currentColor" strokeWidth="4">
-          <circle cx="44" cy="44" r="42" />
-          <path
-            d="M44 88c24.3 0 44-19.7 44-44S68.3 0 44 0 0 19.7 0 44"
-            opacity=".2"
-          />
-          <path
-            d="M44 88c-24.3 0-44-19.7-44-44S19.7 0 44 0"
-            opacity=".2"
-          />
-        </g>
-      </g>
     </svg>
   );
 }
