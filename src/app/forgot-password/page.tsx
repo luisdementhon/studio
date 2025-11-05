a5f3cc1bb63bca931dc257c8a64fe332cc2d7922
Luis Dementhon <luis.dementhon@gmail.com>
2025-11-05 10:04:13 +0000

    Bonjour,
    Le déploiement échoue pendant la phase Framework Build du buil

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -2,7 +2,7 @@
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import type { z } from "zod";
+import { z } from "zod";
 import Link from "next/link";
 import { sendPasswordResetEmail } from "firebase/auth";
 
