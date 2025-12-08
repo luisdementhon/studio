b8d716871fc63b36fa4e1e7fa7c217a8393d554b
Luis Dementhon <luis.dementhon@gmail.com>
2025-12-08 05:52:13 +0000

    centre ce logo et agrabdis la un peu (_for element text_)

--- src/components/dotly-logo.tsx
+++ src/components/dotly-logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export function DotlyLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 300 100"
+      viewBox="0 0 400 120"
       xmlns="http://www.w3.org/2000/svg"
       aria-label="Dotly Logo"
       {...props}
@@ -18,7 +18,7 @@ export function DotlyLogo(props: SVGProps<SVGSVGElement>) {
         fill="currentColor"
         fontFamily="'Playfair Display', serif"
         fontWeight="bold"
-        fontSize="90"
+        fontSize="120"
       >
         Dotly
       </text>
