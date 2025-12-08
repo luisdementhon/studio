8450192a0b2d94fb9947e126e88a898b698bae45
Luis Dementhon <luis.dementhon@gmail.com>
2025-12-08 05:58:16 +0000

    réduis un peu la taille de ces carrés pour le truc donateyrs. piur le me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
                   <Card className="text-left">
                     <CardHeader>
                       <div className="flex items-center gap-4">
-                        <div className="bg-primary/10 text-primary p-3 rounded-lg"><Link2 className="w-7 h-7" /></div>
+                        <div className="bg-primary/10 text-primary p-3 rounded-lg"><Link2 className="w-6 h-6" /></div>
                         <CardTitle className="font-semibold tracking-tight text-xl whitespace-nowrap">Connectez</CardTitle>
                       </div>
                       <CardDescription className="text-muted-foreground pt-4 text-base">Liez votre compte bancaire en quelques clics. Vos données sont chiffrées et protégées.</CardDescription>
@@ -73,7 +73,7 @@ export default function Home() {
                    <Card className="text-left">
                     <CardHeader>
                       <div className="flex items-center gap-4">
-                        <div className="bg-primary/10 text-primary p-3 rounded-lg"><ShoppingCart className="w-7 h-7" /></div>
+                        <div className="bg-primary/10 text-primary p-3 rounded-lg"><ShoppingCart className="w-6 h-6" /></div>
                         <CardTitle className="font-semibold tracking-tight text-xl whitespace-nowrap">Dépensez</CardTitle>
                       </div>
                       <CardDescription className="text-muted-foreground pt-4 text-base">Nous arrondissons automatiquement chaque paiement à l'euro supérieur, sans que vous ayez à y penser.</CardDescription>
@@ -82,7 +82,7 @@ export default function Home() {
                    <Card className="text-left">
                     <CardHeader>
                       <div className="flex items-center gap-4">
-                        <div className="bg-primary/10 text-primary p-3 rounded-lg"><HeartHandshake className="w-7 h-7" /></div>
+                        <div className="bg-primary/10 text-primary p-3 rounded-lg"><HeartHandshake className="w-6 h-6" /></div>
                         <CardTitle className="font-semibold tracking-tight text-xl whitespace-nowrap">Soutenez</CardTitle>
                       </div>
                       <CardDescription className="text-muted-foreground pt-4 text-base">La somme de vos arrondis est reversée aux associations que vous avez choisies. Suivez votre impact en temps réel.</CardDescription>
