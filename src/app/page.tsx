2001f79d70c8ac6180f501c422b58377593871c1
Luis Dementhon <luis.dementhon@gmail.com>
2026-01-14 13:50:23 +0000

    meme dégradé ici  (_for element a_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             </Button>
              <p className="text-base text-muted-foreground">
                 Vous avez déjà un compte ?{' '}
-                <Link href="/login" className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-primary to-red-500 hover:brightness-110 transition-all">
+                <Link href="/login" className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-amber-500 to-yellow-400 hover:brightness-110 transition-all">
                     Se connecter
                 </Link>
             </p>
